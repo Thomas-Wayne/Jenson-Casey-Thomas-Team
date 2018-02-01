02c31f98c46c18add3ded60f9fb669e3410d489d
Thomas-Wayne <lvrsmedic@yahoo.com>
2018-01-31 20:55:10 -0500

    Progress made for team assignment

---
+++ Jenson-Casey-Thomas-Team-Project/src/byui/cit260/MormonTrailProject/model/Player.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.MormonTrailProject.model;
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author Thomas'
+ */
+public class Player implements Serializable {
+    private String name; 
+    private String gender;
+    private String occupation;
+
+    public Player() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + Objects.hashCode(this.name);
+        hash = 41 * hash + Objects.hashCode(this.gender);
+        hash = 41 * hash + Objects.hashCode(this.occupation);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", gender=" + gender + ", occupation=" + occupation + '}';
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.gender, other.gender)) {
+            return false;
+        }
+        if (!Objects.equals(this.occupation, other.occupation)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
---
+++ Jenson-Casey-Thomas-Team-Project/src/jenson/casey/thomas/team/project/CIT260/BYUICIT260MormonTrailProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jenson.casey.thomas.team.project.CIT260;
+
+/**
+ *
+ * @author Jenson, Casey, and Thomas
+ */
+public class BYUICIT260MormonTrailProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
